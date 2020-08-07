8648e804d4e5ed57d8d857856fa7ed86df76c334
abuzeid <abuzeid7@gmail.com>
2020-08-07 17:30:17 +0200

    Initial Commit

---
+++ MusicPlayer/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  MusicPlayer
+//
+//  Created by abuzeid on 07.08.20.
+//  Copyright © 2020 abuzeid. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+  
+
+
+}
+
